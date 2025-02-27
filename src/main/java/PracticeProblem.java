8063774e3a77e62fc5530aeb5babfec86f93dba8
yaseman-nikoo-ics4u-03-25 <yaseman.nikoo@student.tdsb.on.ca>
2025-02-27 19:11:09 +0000

    modify

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,4 +3,37 @@ public class PracticeProblem {
 
 	}
 
+	public static int calculate(int num1, int num2, char operator) {
+		int result = 0;
+		switch (operator) {
+			case '+':
+				result = num1 + num2;
+				break;
+			case '-':
+				result = num1 - num2;
+				break;
+			case '/':
+				result = num1 / num2;
+				break;
+			case '%':
+				result = num1 % num2;
+				break;
+			case '^':
+				result = (int) Math.pow(num1, num2);
+				break;
+		}
+		return result;
+	}
+	final static int  MAXIMUM=10;
+	public static boolean totalWordChecker(String word){
+			
+			boolean answer= false;
+			for (int i =0; i <word.length();i++){
+				if (32<=int(word[i])<=64 || <int(word[i]))
+			}
+							answer = true;
+			
+			return answer;
+		}
 }
+
