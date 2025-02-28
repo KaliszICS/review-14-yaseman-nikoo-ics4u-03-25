a2c9eeeae0b9dd5d32f8344674ba952986fc9f9f
yaseman-nikoo-ics4u-03-25 <yaseman.nikoo@student.tdsb.on.ca>
2025-02-28 19:49:22 +0000

    complete 14

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -12,6 +12,9 @@ public class PracticeProblem {
 			case '-':
 				result = num1 - num2;
 				break;
+			case'*':
+				result= num1*num2;
+				break;
 			case '/':
 				result = num1 / num2;
 				break;
@@ -25,15 +28,26 @@ public class PracticeProblem {
 		return result;
 	}
 	final static int  MAXIMUM=10;
-	public static boolean totalWordChecker(String word){
-			
-			boolean answer= false;
-			for (int i =0; i <word.length();i++){
-				if (32<=int(word[i])<=64 || <int(word[i]))
-			}
-							answer = true;
-			
-			return answer;
-		}
-}
+	public static boolean totalWordsChecker(String word){
+		String[] wordArr =word.split("[^A-Za-z_']+");
+		return wordArr.length<=MAXIMUM;	
+		
+	}
 
+ public static String minString(String word, String word2, String word3){
+	word=word.toLowerCase();
+	word2=word2.toLowerCase();
+	word3=word3.toLowerCase();
+	int num= word.compareTo(word2);
+	int num2= word3.compareTo(word2);
+	if(num>0 && num2>0){
+		return word2;
+	} 
+	else if(num2<num){
+		return word3;
+	}
+	else{
+		return word;
+	}
+ }
+}
